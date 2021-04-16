f94b24d7489de895fdc73475de016bc489269704
Hannes <hannes.nordblom@gmail.com>
2021-04-16 15:50:21 +0200

    ADD: gitignore and indentation
    Co-Authored-By: William Eriksson <54892945+williameriksson126@users.noreply.github.com>

--- app-code/app/src/main/java/com/example/radardisplayapp/DisplayPaperLevel.java
+++ app-code/app/src/main/java/com/example/radardisplayapp/DisplayPaperLevel.java
@@ -16,21 +16,21 @@ public class DisplayPaperLevel extends View {
     private float padding;
 
 
-    public DisplayPaperLevel(Context context, AttributeSet attrs){
-        super(context,attrs);
+    public DisplayPaperLevel(Context context, AttributeSet attrs) {
+        super(context, attrs);
         TypedArray a = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.DisplayPaperLevel,
-                0,0);
+                0, 0);
         try {
             level = a.getInteger(R.styleable.DisplayPaperLevel_disppaperheight, 10);
-        }
-        finally {
+        } finally {
             a.recycle();
         }
         init();
     }
-    private void init(){
+
+    private void init() {
         fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         fillPaint.setColor(0xFFFF0000);
         backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -42,36 +42,35 @@ public class DisplayPaperLevel extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawRect(0,0,getWidth(),getHeight(),backgroundPaint);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), backgroundPaint);
 
-        float colorNum = level/100;
-        float s =1.75f;
+        float colorNum = level / 100;
+        float s = 1.75f;
 
-        int green = (int)  ((255) * (colorNum*s));
-        int red = (int) ( (255) * (colorNum * (s-2) + 1 ));
+        int green = (int) (255 * (colorNum * s));
+        int red = (int) (255 * (colorNum * (s - 2) + 1));
         int blue = 0;
 
-        if(colorNum>0.5) {
-            green = (int) ((255) * (colorNum*(2-s)+(s-1)));
-            red = (int) ( (255) * ( (colorNum-1) * (-s) ));
+        if (colorNum > 0.5) {
+            green = (int) (255 * (colorNum * (2 - s) + (s - 1)));
+            red = (int) (255 * ((colorNum - 1) * (-s)));
         }
-        fillPaint.setARGB(255,red,green,blue);
+        fillPaint.setARGB(255, red, green, blue);
 
-        canvas.drawRect(padding,padding,getWidth()-padding,getHeight()-padding,innerBackground);
-        canvas.drawRect(padding,padding + heightFromLevel(),getWidth()-padding,getHeight()-padding,fillPaint);
-        canvas.drawText("g:"+green+", r:"+red+", b:"+blue+", with cNum:"+colorNum,30,30,backgroundPaint);
+        canvas.drawRect(padding, padding, getWidth() - padding, getHeight() - padding, innerBackground);
+        canvas.drawRect(padding, padding + heightFromLevel(), getWidth() - padding, getHeight() - padding, fillPaint);
+        canvas.drawText("g:" + green + ", r:" + red + ", b:" + blue + ", with cNum:" + colorNum, 30, 30, backgroundPaint);
     }
 
-    private float heightFromLevel()
-    {
-        float full = Math.max(getHeight() - 40,0);
+    private float heightFromLevel() {
+        float full = Math.max(getHeight() - 40, 0);
 
-        return full * (1-level/100);
+        return full * (1 - level / 100);
     }
 
-    public void setLevel(float newLevel){
+    public void setLevel(float newLevel) {
         level = newLevel;
         invalidate();
     }
